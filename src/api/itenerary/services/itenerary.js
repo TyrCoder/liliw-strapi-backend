5168fc7d1f1ed959c7f15db6f12a44ea3a6a4a5e
Timothy <Tbalbierani@gmail.com>
2026-05-19 12:21:19 +0800

    fix: rename itenerary collection to itinerary (correct spelling)

--- src/api/itenerary/services/itenerary.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * itenerary service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::itenerary.itenerary');
